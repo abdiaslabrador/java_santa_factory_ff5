34f1db37aa591d6864222adb6f43da7bf3a751b7
abdias labrador <abdiaslabrador@gmail.com>
2024-11-14 20:33:22 +0100

    fix: GoodToyRepository interface with the methods implemented but not developed

--- src/main/java/dev/proyect/santa_factory/repositories/GoodToyRepository.java
+++ src/main/java/dev/proyect/santa_factory/repositories/GoodToyRepository.java
@@ -1,5 +1,34 @@
 package dev.proyect.santa_factory.repositories;
 
-public class GoodToyRepository {
+import java.util.List;
+
+import dev.proyect.santa_factory.interfaces.InterfaceRepository;
+import dev.proyect.santa_factory.models.Toy; 
+
+public class GoodToyRepository implements InterfaceRepository {
+
+    @Override
+    public void save() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public List<Toy> delete() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public Toy getOne() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOne'");
+    }
+
+    @Override
+    public List<Toy> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
     
 }
